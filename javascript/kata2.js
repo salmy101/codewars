81cfd33d86c5f549dee63e2b74621d8fd9b95b83
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-13 15:26:33 -0700

    kata 2 solved, kata 3 in progress

---
+++ javascript/kata2.js
@@ -0,0 +1,13 @@
+//You Can't Code Under Pressure #1
+
+
+function doubleInteger(i) {
+  //Code as fast as you can! You need to double the integer and return it.
+  return;
+}
+
+
+function doubleInteger(i) {
+  
+  return i * 2;
+}
