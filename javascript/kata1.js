f9ff1f5a9fd0b2a0e293f8fb325887d3227c4ff6
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-12 23:40:41 -0700

    solved kata1

---
+++ javascript/kata1.js
@@ -0,0 +1,11 @@
+//Convert boolean values to strings 'Yes' or 'No'.
+//if the boolean is true return 'Yes' and if its false, return 'No'
+
+
+function boolToWord( bool ){
+  if(bool === true) {
+    return 'Yes'
+  } else {
+    return 'No'
+  }
+}
