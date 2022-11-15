91b5c5f977aab5dc36aea426d2a4ae8a43bb67e8
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-15 12:29:01 -0700

    kata 4 complete

---
+++ javascript/kata4.js
@@ -0,0 +1,12 @@
+//Given an array of integers, return a new array with each value doubled.
+
+/*For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+
+
+
+function maps(x){
+  return x.map(el => el * 2) // the map() method will alter each element in an array,
+
+}
