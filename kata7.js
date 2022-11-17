4daea4494d178beae76cbf0c384e9eac6353391f
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-16 19:24:58 -0700

    kata 7 complete

--- kata7.js
+++ kata7.js
@@ -6,10 +6,10 @@ Create a function named repeatNumbers that will return a string with each of the
 
  const repeatNumbers = function(data) {
   // Put your solution here
-  console.log(data[0][0].toString())
+     data.forEach(el => console.log(el[0].toLocaleString().repeat(el[1])))
 
 };
 
 console.log(repeatNumbers([[1, 10]])); // 1111111111
 console.log(repeatNumbers([[1, 2], [2, 3]])); // 11, 222
-console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2], [6,8]]));// 10101010, 343434343434, 9292
