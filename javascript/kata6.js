7a85b35fba1f1a5f8ca936c8db768c34243f9325
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-18 15:21:09 -0700

    complete kata6

--- javascript/kata6.js
+++ javascript/kata6.js
@@ -5,11 +5,16 @@ Complete the solution so that it reverses the string passed into it.
 'word'   =>  'drow'
  */
 
-function solution(str){
-  const splitString = str.split("")
-  const revString = splitString.reverse()
-  const joinString = revString.join("")
-  return joinString;
+// function solution(str){
+//   const splitString = str.split("")
+//   const revString = splitString.reverse()
+//   const joinString = revString.join("")
+//   return joinString;
 
   
+// }
+
+function solution(str){
+  return str.split("").reverse().join("")
+
 }
