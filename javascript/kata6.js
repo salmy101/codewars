4af3a2b18b0baeac1c784694422b73a1d003c959
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-18 15:19:31 -0700

    complete kata6

--- javascript/kata6.js
+++ javascript/kata6.js
@@ -4,3 +4,12 @@ Complete the solution so that it reverses the string passed into it.
 'world'  =>  'dlrow'
 'word'   =>  'drow'
  */
+
+function solution(str){
+  const splitString = str.split("")
+  const revString = splitString.reverse()
+  const joinString = revString.join("")
+  return joinString;
+
+  
+}
---
+++ javascript/kata8.js
@@ -0,0 +1,15 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const mapFunc = function(arr) {
+   return arr.map((word) => word.length )
+}
+
+console.log(mapFunc(lighthouses))
+
+const tech = ["Women in Tech", "Javascript", "Shmoney", "Go Getters"];
+
+const mapFunc2 = function(arr) {
+   return arr.map((word) => word.length )
+}
+
+console.log(mapFunc2(tech))
