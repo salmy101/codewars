70c17ca0b0b79f20b7c5014c0f9670370e8fbef0
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-19 20:50:43 -0700

    complete kata12

--- javascript/kata11.js
+++ javascript/kata11.js
@@ -10,4 +10,6 @@ Example(Input => Output):
 function digitize(n) {
   //code here
   return n.toString().split("").reverse().map(Number);
+  // Calls Number on each value in the array (casting it to a number)
+              // and returns the array of results. [1,2,3,4]
 }
