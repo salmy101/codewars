6e3fd5bfd55f0c49db02106ea2fce716410b4826
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-01-25 18:46:25 -0700

    leetcode3

---
+++ javascript/leetcode3.js
@@ -0,0 +1,31 @@
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+ */
+
+var isSubsequence = function(s, t) {
+  for (let i = 0; i < s.length; i++) {
+    for (let l = 0; l < t.length; t++){
+      if (t.includes(s[i])) {
+        return true;
+      }
+
+    }
+  }
+    
+};
+
+isSubsequence("abc","ahbgdc" );
