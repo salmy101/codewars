b620e6c984b3b8f3e84f6bff16c6849508718784
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-20 23:52:44 -0700

    kata #3

--- prep_module/kata2.js
+++ prep_module/kata2.js
@@ -5,7 +5,6 @@ const multiplicationTable = function(max){
   for (let i = 1; i <= max; i++){ // the outer loop is stuck until inner ges through every possible iteration
     for (let k = 1; k <= max; k++){ //once this is done, it jumps out loop and i becomes 2, starts over till k is 5, jumps out again
       // console.log(`this is i: ${i} and this is k: ${k}`) // 1 1 , 1 2, 1 3...2 1, 2 2, 2 3, ..., 5 5
-      
       line += (i * k)  + ' '
     }
   }
