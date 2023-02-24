dc12ea6fb7d2efaf23c2a2225a1141a9dcd4a47f
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-02-23 23:26:09 -0700

    technical mock

--- javascript/isValidSequence.js
+++ javascript/isValidSequence.js
@@ -12,3 +12,19 @@ sequence = [1, 6, -1, 10]
 *Sample Output*
 true
 */
+
+
+
+const arr1 = [5, 1, 22, 25, 6, -1, 8, 10]
+const arr2 = [1, 6, -1, 10]
+
+
+const isValidSubsequence = function(arr1, arr2){
+  let pointer = 0;
+  
+  for (let i = 0; i < arr1.length ; i++) {
+    console.log(arr1[i], arr2[i])
+  }
+}
+
+console.log(isValidSubsequence(arr1, arr2))
